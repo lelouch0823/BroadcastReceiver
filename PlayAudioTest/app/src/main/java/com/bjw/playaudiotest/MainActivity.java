c1fb44d540a241905f92cc4022ee326e026290d2
lelouch0823 <lelouch0823@hotmail.com>
2016-07-10 20:06:24 +0800

    简易音乐播放App

---
+++ PlayAudioTest/app/src/main/java/com/bjw/playaudiotest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.bjw.playaudiotest;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.orhanobut.logger.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.play)
+    Button play;
+    @BindView(R.id.pause)
+    Button pause;
+    @BindView(R.id.stop)
+    Button stop;
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initMediaPlayer();
+    }
+
+    private void initMediaPlayer() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "hah.mp3");
+            mediaPlayer.setDataSource(file.getPath());
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Logger.d("start");
+        }
+    }
+
+    @OnClick({R.id.play, R.id.pause, R.id.stop})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.play:
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                    Logger.d("start");
+                }
+                break;
+            case R.id.pause:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.stop:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+    }
+}
